417412d97c1f8ab1dad2d28ed928cdb4ef10bae0
Paul Iogna <paul.iogna@gmail.com>
2016-02-05 14:14:47 -0500

    implements associations for all models and validations for workout

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,12 @@
 class Workout < ActiveRecord::Base
   has_many :workout_exercises, dependent: :destroy
+
+  validates :date, presence: true
+
+  validate :created_in_past_or_present
+
+  private
+  def created_in_past_or_present
+    errors.add(:date, "Date must not be in the future") unless date < DateTime.now
+  end
 end
--- app/models/workout_exercise.rb
+++ app/models/workout_exercise.rb
@@ -1,3 +1,4 @@
 class WorkoutExercise < ActiveRecord::Base
+  has_many :ex_sets
   belongs_to :workout
 end
