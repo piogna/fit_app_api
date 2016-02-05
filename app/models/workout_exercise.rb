6570777f773a0ebe20a0edcf7f6f3700a02bf361
Paul Iogna <paul.iogna@gmail.com>
2016-02-05 14:20:44 -0500

    adds validations for WorkoutExercise

--- app/models/workout_exercise.rb
+++ app/models/workout_exercise.rb
@@ -1,4 +1,7 @@
 class WorkoutExercise < ActiveRecord::Base
-  has_many :ex_sets
+  has_many :ex_sets, dependent: :destroy
   belongs_to :workout
+
+  validates :order, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :exercise_name, presence: true
 end
